f11fcbb1f1c34ab3dfc341d5aae9bb2c1c3b8293
BOZO THE GEEK <bozolesv2@gmail.com>
2026-08-16 02:14:50 +0200

    patch(pixl): 0001-fileutil-change-default-path.patch

--- Source/Core/Common/FileUtil.cpp
+++ Source/Core/Common/FileUtil.cpp
@@ -836,9 +836,9 @@ static void RebuildUserDirectories(unsigned int dir_index)
   switch (dir_index)
   {
   case D_USER_IDX:
-    s_user_paths[D_GCUSER_IDX] = s_user_paths[D_USER_IDX] + GC_USER_DIR DIR_SEP;
-    s_user_paths[D_WIIROOT_IDX] = s_user_paths[D_USER_IDX] + WII_USER_DIR DIR_SEP;
-    s_user_paths[D_TRIUSER_IDX] = s_user_paths[D_USER_IDX] + TRI_USER_DIR DIR_SEP;
+    s_user_paths[D_GCUSER_IDX] = "/recalbox/share/saves/gamecube/";
+    s_user_paths[D_WIIROOT_IDX] = "/recalbox/share/saves/wii/";
+    s_user_paths[D_TRIUSER_IDX] = "/recalbox/share/saves/triforce/";
     s_user_paths[D_CONFIG_IDX] = s_user_paths[D_USER_IDX] + CONFIG_DIR DIR_SEP;
     s_user_paths[D_GAMESETTINGS_IDX] = s_user_paths[D_USER_IDX] + GAMESETTINGS_DIR DIR_SEP;
     s_user_paths[D_MAPS_IDX] = s_user_paths[D_USER_IDX] + MAPS_DIR DIR_SEP;
@@ -850,7 +850,7 @@ static void RebuildUserDirectories(unsigned int dir_index)
         s_user_paths[D_CACHE_IDX] + RETROACHIEVEMENTSCACHE_DIR DIR_SEP;
     s_user_paths[D_SHADERS_IDX] = s_user_paths[D_USER_IDX] + SHADERS_DIR DIR_SEP;
     s_user_paths[D_STATESAVES_IDX] = s_user_paths[D_USER_IDX] + STATESAVES_DIR DIR_SEP;
-    s_user_paths[D_SCREENSHOTS_IDX] = s_user_paths[D_USER_IDX] + SCREENSHOTS_DIR DIR_SEP;
+    s_user_paths[D_SCREENSHOTS_IDX] = "/recalbox/share/screenshots/";
     s_user_paths[D_LOAD_IDX] = s_user_paths[D_USER_IDX] + LOAD_DIR DIR_SEP;
     s_user_paths[D_HIRESTEXTURES_IDX] = s_user_paths[D_LOAD_IDX] + HIRES_TEXTURES_DIR DIR_SEP;
     s_user_paths[D_RIIVOLUTION_IDX] = s_user_paths[D_LOAD_IDX] + RIIVOLUTION_DIR DIR_SEP;
